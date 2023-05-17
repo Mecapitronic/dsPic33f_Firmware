fd824c5e9b4edc5afa29614ed22c2ab780f3f859
Geoffroy <geoarenou@gmail.com>
2023-05-17 19:38:38 +0200

    uart obs

--- pilot.x/Uart.c
+++ pilot.x/Uart.c
@@ -201,7 +201,7 @@ void Update_UART1(void)
 	Write_UART1(';');
 	Write_Float_UART1(RAD_TO_DEG(robot.rad), 0);
 	Write_UART1(';');
-
+	/*
 	Write_Int_UART1(move_lin.command.position);
 	Write_UART1(';');
 	Write_Int_UART1(move_lin.command.velocity);
@@ -213,7 +213,8 @@ void Update_UART1(void)
 	Write_Int_UART1(robot.lin.velocity);
 	Write_UART1(';');
 	Write_Int_UART1(robot.lin.acceleration);
-	/*
+	*/
+	
 	Write_Int_UART1(Get_Obstacle(0).p.x);
 	Write_UART1(',');
 	Write_Int_UART1(Get_Obstacle(0).p.y);
@@ -225,7 +226,7 @@ void Update_UART1(void)
 	Write_Int_UART1(Get_Obstacle(1).p.y);
 	Write_UART1(',');
 	Write_Int_UART1(Get_Obstacle(1).r);
-	*/
+	
 	/*
 	// Robot Start/End path
 	Write_Int_UART1(vertex[0].point.x);
