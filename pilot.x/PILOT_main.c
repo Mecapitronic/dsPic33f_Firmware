b7343e6f1478cc25f0677c0501b64efbac808e35
PercevalSA <perceval.arenou@gmail.com>
2023-05-18 12:29:59 +0200

    put cakes into plate

--- pilot.x/PILOT_main.c
+++ pilot.x/PILOT_main.c
@@ -12,11 +12,11 @@
 #include "PILOT_main.h"
 
 #ifdef SERIAL_PRINT
-#pragma message (" Serial Print is enabled ! ")
+#pragma message(" Serial Print is enabled ! ")
 #endif
 
 #ifdef __MPLAB_SIMULATION__
-#pragma message (" Timer at 0 for Simulation with MPLAB ! ")
+#pragma message(" Timer at 0 for Simulation with MPLAB ! ")
 #endif
 
 /****************************************************************************************
@@ -30,13 +30,14 @@ uint8 recalage = FALSE;
 /****************************************************************************************
  * Routine Principale
  ****************************************************************************************/
-_PILOT_{
+_PILOT_
+{
     current_time = 0;
     Setup_Hardware();
-    
-    //desactivate servo
+
+    // desactivate servo
     DESACTIVER_BRAS();
-      
+
     Sequence_LCD_Initiale();
     Sequence_LED_Initiale();
 
@@ -44,7 +45,7 @@ _PILOT_{
     Setup_Timer_Secondaire();
     Set_Timer_Primaire(ON);
     Set_Timer_Secondaire(ON);
-    
+
     Start_UART1();
     Start_UART2();
 
@@ -58,7 +59,8 @@ _PILOT_{
             mode = MODE_PILOT;
             start = START_PILOT;
             recalage = RECALAGE_PILOT;
-            if (timeLCD > 250) {
+            if (timeLCD > 250)
+            {
                 LED_Toggle();
                 Sequence_LCD_Waiting_Mode();
                 Sequence_LCD_Waiting_Start();
@@ -67,10 +69,10 @@ _PILOT_{
             timeLCD++;
             Delay_Ms(1);
         }
-        
-    // le robot est face contre le panier, avec une calle sur le coté entre la bordure et lui
-    // valeur entre le milieu du robot et le panier : 114mm, entre le milieu du robot et la bordure : 225mm
-    // Au recalage : init position, recule, monte la pince, désactive les servos et avance au maximum vers le panier
+
+        // le robot est face contre le panier, avec une calle sur le cotï¿½ entre la bordure et lui
+        // valeur entre le milieu du robot et le panier : 114mm, entre le milieu du robot et la bordure : 225mm
+        // Au recalage : init position, recule, monte la pince, dï¿½sactive les servos et avance au maximum vers le panier
 
         if (team == TEAM_A)
             Initialize_Robot_Position(1775, 2850, 90);
@@ -78,31 +80,35 @@ _PILOT_{
             Initialize_Robot_Position(225, 2850, 90);
 
         Translate(-150, SPEED_LIN);
-        while (Wait_Trajectory()) {
+        while (Wait_Trajectory())
+        {
             Display();
         };
-        Delay_Ms(500);    
+        Delay_Ms(500);
 
         DEPOSER_BRAS();
         Delay_Ms(2000);
 
-        //desactivate servo
+        // desactivate servo
         DESACTIVER_BRAS();
 
-        Translate(150, 400);    
+        Translate(150, 400);
         Delay_Ms(3000);
         Translate(0, 400);
-    
-        while (START_PILOT){}
+
+        while (START_PILOT)
+        {
+        }
     }
-    
-    while (!START_PILOT) // Attente de démarrage du copilot 
+
+    while (!START_PILOT) // Attente de dï¿½marrage du copilot
     {
         team = SELECT;
         mode = MODE_PILOT;
         start = START_PILOT;
         recalage = RECALAGE_PILOT;
-        if (timeLCD > 250) {
+        if (timeLCD > 250)
+        {
             LED_Toggle();
             Sequence_LCD_Waiting_Mode();
             Sequence_LCD_Waiting_Start();
@@ -116,9 +122,9 @@ _PILOT_{
     DEPOSER_BRAS();
     Delay_Ms(2000);
 
-    //desactivate servo
+    // desactivate servo
     DESACTIVER_BRAS();
-    
+
     // Initialisations
     if (team == TEAM_A)
         Initialize_Robot_Position(1775, 2775, 90);
@@ -131,133 +137,162 @@ _PILOT_{
 
     Initialize_Action();
 
-    if (mode == MODE_MATCH){
+    if (mode == MODE_MATCH)
+    {
 
-        //Dépose des cerises pré-embarquées
+        // Dï¿½pose des cerises prï¿½-embarquï¿½es
         SetAntiSlip(PWM_MIN_SLIP + 50);
-        Translate(120, SPEED_LIN/4);
+        Translate(120, SPEED_LIN / 4);
         Delay_Ms(2000);
-        
-        //ResetAntiSlip();
 
-        //Marche arrière
-        Translate(-120, SPEED_LIN/2);
+        // ResetAntiSlip();
+
+        // Marche arriï¿½re
+        Translate(-120, SPEED_LIN / 2);
         While_Trajectory(Display);
 
-        //Rotation pour descendre le bras
+        // Rotation pour descendre le bras
         {
             Rotate_To_Angle(270, SPEED_ANG);
             While_Trajectory(Display);
         }
-        
-        //Descente du bras en position d'attente
+
+        // Descente du bras en position d'attente
         PREPARER_BRAS();
         Delay_Ms(2000);
 
-        //Orientation vers cerises
+        // Orientation vers cerises
         {
-            t_point p = {0,0};
+            t_point p = {0, 0};
             if (team == TEAM_A)
                 p.x = vertex[8].point.x;
             else
                 p.x = vertex[5].point.x;
-            
-            p.y = 2800; 
-            
+
+            p.y = 2800;
+
             float distance = Get_Distance_Point(&robot.mm, &p);
-            Translate(-distance, SPEED_LIN/2);
-            
+            Translate(-distance, SPEED_LIN / 2);
+
             While_Trajectory(Display);
-            t_point p2 = { 1000,2850 };
+            t_point p2 = {1000, 2850};
             Rotate_To_Point(p2, SPEED_ANG);
             While_Trajectory(Display);
         }
-        
-        //Déplacement vers cerises
+
+        // Dï¿½placement vers cerises
         {
-            t_point p = { 1000,2850 };
+            t_point p = {1000, 2850};
             if (team == TEAM_A)
-                p.x+=90;
+                p.x += 90;
             else
-                p.x-=90;
-            Rotate_To_Point(p, SPEED_ANG/2);
+                p.x -= 90;
+            Rotate_To_Point(p, SPEED_ANG / 2);
             While_Trajectory(Display);
-            SetAntiSlip(PWM_MIN_SLIP+50);
+            SetAntiSlip(PWM_MIN_SLIP + 50);
             float32 distance = Get_Distance_Point(&robot.mm, &p);
-            Translate(distance, SPEED_LIN/4);            
+            Translate(distance, SPEED_LIN / 4);
             Delay_Ms(5000);
-            
-            //Prise des cerises
+
+            // Prise des cerises
             PRISE_BRAS();
             Delay_Ms(2000);
 
-            //Position d'attente
+            // Position d'attente
             PREPARER_BRAS();
             Delay_Ms(2000);
 
             // recul
-            Translate(-distance/4, SPEED_LIN/2);            
+            Translate(-distance / 4, SPEED_LIN / 2);
             Delay_Ms(5000);
             ResetAntiSlip();
         }
 
-
-
-        //Retour au panier
-        while (!Execute_Action(1));        
+        // Retour au panier et dÃ©pose cerises
+        while (!Execute_Action(1))
+            ;
         PREPARER_BRAS();
 
-        
-        SetAntiSlip(PWM_MIN_SLIP+1000);
-        
-        Rotate_To_Angle(0,SPEED_ANG);
+        SetAntiSlip(PWM_MIN_SLIP + 1000);
+
+        // allons chercher des gateaux pour les mettre dans notre zone
+        // avancer
+        {
+            t_point zone_depose_gateaux = {0, 1350};
+            // Orientation vers gateaux
+            if (team == TEAM_A)
+                zone_depose_gateaux.x = 1775;
+            else
+                zone_depose_gateaux.x = 225;
+            Rotate_To_Point(zone_depose_gateaux, SPEED_ANG);
+            While_Trajectory(Display);
+
+            // on avance
+            float distance = Get_Distance_Point(&robot.mm, &p);
+            Translate(distance, SPEED_LIN / 2);
+
+            // et on recul pour pas abimer les gateaux dans l'assiette
+            distance = -100;
+            Translate(distance, SPEED_LIN / 2);
+            While_Trajectory(Display);
+        }
+
+        // danse de la victoire
+        Rotate_To_Angle(0, SPEED_ANG);
         Delay_Ms(2000);
-        Rotate_To_Angle(180,SPEED_ANG);
+        Rotate_To_Angle(180, SPEED_ANG);
         Delay_Ms(2000);
-        Rotate_To_Angle(0,SPEED_ANG);
+        Rotate_To_Angle(0, SPEED_ANG);
         Delay_Ms(2000);
-        Rotate_To_Angle(180,SPEED_ANG);
+        Rotate_To_Angle(180, SPEED_ANG);
         Delay_Ms(3000);
         ResetAntiSlip();
 
-        //Prise balles coté départ
-        //while (!Execute_Action(2));
+        // Prise balles cotï¿½ dï¿½part
+        // while (!Execute_Action(2));
 
-        //Retour au panier
-        //while (!Execute_Action(1));
+        // Retour au panier
+        // while (!Execute_Action(1));
 
-        //Prise balles coté adverse
-        //while (!Execute_Action(3));
+        // Prise balles cotï¿½ adverse
+        // while (!Execute_Action(3));
 
-        //Retour au panier
-        //while (!Execute_Action(1));
+        // Retour au panier
+        // while (!Execute_Action(1));
 
-
-        //action[1].iteration = 3;
-        //while (!Execute_Action(1));
-        //while (!Execute_Action(2));
-        //while (!Execute_Action(3));
-        //while (!Execute_Action(4));
-
-    }else{
-        while (FOREVER) {
-            if (uartCMD.cmd != '0') {
-                switch (uartCMD.cmd) {
+        // action[1].iteration = 3;
+        // while (!Execute_Action(1));
+        // while (!Execute_Action(2));
+        // while (!Execute_Action(3));
+        // while (!Execute_Action(4));
+    }
+    else
+    {
+        while (FOREVER)
+        {
+            if (uartCMD.cmd != '0')
+            {
+                switch (uartCMD.cmd)
+                {
                 case 'A':
                     Send_UART1_ACK(CMD_BUSY);
-                    if (Execute_Action(uartCMD.actionID)) {
+                    if (Execute_Action(uartCMD.actionID))
+                    {
                         Send_UART1_ACK(CMD_DONE);
                     }
-                    else {
+                    else
+                    {
                         Send_UART1_ACK(CMD_FAIL);
                     }
                     break;
                 case 'V':
                     Send_UART1_ACK(CMD_BUSY);
-                    if (Navigate_To_Vertex(uartCMD.vertexID, 10)) {
+                    if (Navigate_To_Vertex(uartCMD.vertexID, 10))
+                    {
                         Send_UART1_ACK(CMD_DONE);
                     }
-                    else {
+                    else
+                    {
                         Send_UART1_ACK(CMD_FAIL);
                     }
                     break;
@@ -273,12 +308,14 @@ _PILOT_{
                     p.x = uartCMD.point.x;
                     p.y = uartCMD.point.y;
                     Rotate_To_Point(p, SPEED_ANG);
-                    while (Wait_Trajectory()) {
+                    while (Wait_Trajectory())
+                    {
                         Display();
                     };
                     uint32 distance = Get_Distance_Point(&robot.mm, &p);
                     Translate(distance, SPEED_LIN);
-                    while (Wait_Trajectory()) {
+                    while (Wait_Trajectory())
+                    {
                         Display();
                     };
                     Send_UART1_ACK(CMD_DONE);
@@ -286,7 +323,8 @@ _PILOT_{
                 case 'T':
                     Send_UART1_ACK(CMD_BUSY);
                     Translate(uartCMD.distance, SPEED_LIN);
-                    while (Wait_Trajectory()) {
+                    while (Wait_Trajectory())
+                    {
                         Display();
                     };
                     Send_UART1_ACK(CMD_DONE);
@@ -294,7 +332,8 @@ _PILOT_{
                 case 'R':
                     Send_UART1_ACK(CMD_BUSY);
                     Rotate_To_Angle(uartCMD.angle, SPEED_ANG);
-                    while (Wait_Trajectory()) {
+                    while (Wait_Trajectory())
+                    {
                         Display();
                     };
                     Send_UART1_ACK(CMD_DONE);
@@ -308,13 +347,16 @@ _PILOT_{
         }
     }
 
-    //#define RECHERCHE_ACTION
+    // #define RECHERCHE_ACTION
 #ifdef RECHERCHE_ACTION
     int8 action = 0;
-    while (action != -1) {
-        action = GetBestStrategiePossible(); // choix de l'action à effectuer
-        if (action != -1) {
-            while (Executer_Strategie(action) != OK); // execution de l'action
+    while (action != -1)
+    {
+        action = GetBestStrategiePossible(); // choix de l'action ï¿½ effectuer
+        if (action != -1)
+        {
+            while (Executer_Strategie(action) != OK)
+                ; // execution de l'action
         }
     }
 #endif
@@ -331,7 +373,8 @@ _PILOT_{
     //  }
 
     // Do not restart the main
-    while (FOREVER);
+    while (FOREVER)
+        ;
 
     return 1;
 }
@@ -342,17 +385,18 @@ _PILOT_{
 int32 running = 0;
 int32 coef = 32;
 
-void Display() {
+void Display()
+{
     // robot position
     LCD_Line(1);
     LCD_Text("X", 1);
     LCD_Value(robot.mm.x, 4, 0);
-	LCD_Text(" Y", 2);
+    LCD_Text(" Y", 2);
     LCD_Value(robot.mm.y, 4, 0);
     LCD_Text(" ", 1);
     LCD_Value(robot.deg, 3, 0);
     LCD_Char(0xDF); // "degre" character
-    //LCD_Text(" ", 1);
+    // LCD_Text(" ", 1);
     if (running < coef)
         LCD_Text("|", 1);
     else if (running < coef * 2)
@@ -361,42 +405,43 @@ void Display() {
         LCD_Text("-", 1);
     else if (running < coef * 4)
         LCD_Text("\\", 1);
-    //LCD_Char(0x5C); // '\' char
+    // LCD_Char(0x5C); // '\' char
 
     running++;
     if (running > coef * 4)
         running = 0;
 
-    //Display_Sharp(2);
+    // Display_Sharp(2);
 
-	// odometry
-    //LCD_Line(2);
-	//LCD_Text("R", 1);
-	//LCD_Value(wheel_right.velocity, 7, 0);
-	//LCD_Text("L", 1);
-	//LCD_Value(wheel_left.velocity, 7, 0);
+    // odometry
+    // LCD_Line(2);
+    // LCD_Text("R", 1);
+    // LCD_Value(wheel_right.velocity, 7, 0);
+    // LCD_Text("L", 1);
+    // LCD_Value(wheel_left.velocity, 7, 0);
 
     // command velocity
-    //LCD_Line(3);
-    //LCD_Text("cVL", 3);
-    //LCD_Value(move_lin.command.velocity, 6, 0);
-    //LCD_Text("  cVA", 5);
-    //LCD_Value(move_ang.command.velocity, 6, 0);
+    // LCD_Line(3);
+    // LCD_Text("cVL", 3);
+    // LCD_Value(move_lin.command.velocity, 6, 0);
+    // LCD_Text("  cVA", 5);
+    // LCD_Value(move_ang.command.velocity, 6, 0);
 
-    //Afficher_UART(3);
+    // Afficher_UART(3);
 
     // UART Receive
-    //Afficher_UART2(2);
-    
-    Sequence_LCD_Score();
+    // Afficher_UART2(2);
 
+    Sequence_LCD_Score();
 }
 
 /****************************************************************************************
- * Séquences au démarrage du système
+ * Sï¿½quences au dï¿½marrage du systï¿½me
  ****************************************************************************************/
-void Sequence_LED_Initiale(void) {
-    for (uint8 i = 0; i < 5; i++) {
+void Sequence_LED_Initiale(void)
+{
+    for (uint8 i = 0; i < 5; i++)
+    {
         LED = !LED;
         Delay_Ms(100);
     }
@@ -405,7 +450,8 @@ void Sequence_LED_Initiale(void) {
     LED = OFF;
 }
 
-void Sequence_LCD_Initiale(void) {
+void Sequence_LCD_Initiale(void)
+{
     LCD_Line(1);
     LCD_Text("GR 2023", LCD_NB_CHARS);
 
@@ -416,36 +462,44 @@ void Sequence_LCD_Initiale(void) {
     LCD_Text(__TIME__, 8);
 }
 
-void Sequence_LCD_Waiting_Mode(void) {
+void Sequence_LCD_Waiting_Mode(void)
+{
     LCD_Line(1);
     LCD_Text("PILOT ", 6);
-    if (MODE_PILOT == MODE_MATCH) LCD_Text("Match ", 6);
-    else LCD_Text("Test  ", 6);
-    if (SELECT == TEAM_A) LCD_Text(LCD_TEAM_A, 4);
-    else LCD_Text(LCD_TEAM_B, 4);
+    if (MODE_PILOT == MODE_MATCH)
+        LCD_Text("Match ", 6);
+    else
+        LCD_Text("Test  ", 6);
+    if (SELECT == TEAM_A)
+        LCD_Text(LCD_TEAM_A, 4);
+    else
+        LCD_Text(LCD_TEAM_B, 4);
 }
 
 uint8 startLCD = 0;
-void Sequence_LCD_Waiting_Start(void) {
+void Sequence_LCD_Waiting_Start(void)
+{
     LCD_Line(2);
-    //for (uint8 i = 0; i < 1; i++) {
-        if (startLCD == 0)
-            LCD_Char('>');
-        else
-            LCD_Char(' ');
+    // for (uint8 i = 0; i < 1; i++) {
+    if (startLCD == 0)
+        LCD_Char('>');
+    else
+        LCD_Char(' ');
     //}
-    if (recalage == TRUE) LCD_Text("Calage Bordure", 14);
-    else LCD_Text("Attente  Start", 14);
+    if (recalage == TRUE)
+        LCD_Text("Calage Bordure", 14);
+    else
+        LCD_Text("Attente  Start", 14);
 
-    //for (uint8 i = 0; i < 1; i++) {
-        //LCD_Goto(4, 15);
-        if (startLCD == 0)
-            LCD_Char('<');
-        else
-            LCD_Char(' ');
+    // for (uint8 i = 0; i < 1; i++) {
+    // LCD_Goto(4, 15);
+    if (startLCD == 0)
+        LCD_Char('<');
+    else
+        LCD_Char(' ');
     //}
     startLCD++;
-    if (startLCD >1)
+    if (startLCD > 1)
         startLCD = 0;
 }
 
@@ -454,4 +508,3 @@ void Sequence_LCD_Score()
     LCD_Line(2);
     LCD_Text(" Score : 35 Pts ", 16);
 }
-
